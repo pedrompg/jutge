8bf2d831dd7129c7816c676a37572d863e303f2b
pedrompg <pedrompg@gmail.com>
2013-09-07 23:31:29 +0200

    Renamed fodler to match problem names

--- cpp/introduction/P37469_ca/S001-AC.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int x;
-	cin >> x;
-	cout << x/3600 << " " << x/60%60 << " " << x%60 << endl;
-}
--- cpp/introduction/P92351_ca/S003-CE.py
+++
@@ -1,8 +0,0 @@
-import os, re, sys
-
-x = raw_input('')
-y = raw_input('')
-x = int (x)
-y = int (y)
-print x/y
-print x%y
